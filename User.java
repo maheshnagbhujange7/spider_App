430f1d83493fc5320d189a8d8d871ef8fa2d204a
Mahesh Nagbhujange <138371017+maheshnagbhujange7@users.noreply.github.com>
2025-08-16 19:23:44 +0530

    Update User.java

--- User.java
+++ User.java
@@ -3,21 +3,6 @@ class Mahesh{
  public static void main(String [] args){
 
   System.out.println("Mahesh 007");
-
-
-
-//Bug(103)
-System.out.println("Maximus Decimous Meradious");
-
-
-//Bug (102)
-System.out.println("");
-
-//BUg (104)
-"HI";
-
-//bug 105
-"Bye";
   
  }
  
